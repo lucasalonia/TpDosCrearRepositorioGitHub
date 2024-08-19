b188a73ada3008996981df76db469432b29bbb1a
lucasalonia <salonialuca97@gmail.com>
2024-08-19 16:06:43 -0300

    Initial commit

---
+++ src/tpdoscrearrepositoriogithub/TpDosCrearRepositorioGitHub.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpdoscrearrepositoriogithub;
+
+/**
+ *
+ * @author salon
+ */
+public class TpDosCrearRepositorioGitHub {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Testeo control del versiones trabajo grupal. -Linea cargada por Luca");
+        
+    }
+    
+}
